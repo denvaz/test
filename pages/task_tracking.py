3fe85035b70058457838551b96ae80e97f6afc94
denvaz <65169169+denvaz@users.noreply.github.com>
2024-07-31 21:14:44 -0500

    Add files via upload

---
+++ pages/task_tracking.py
@@ -0,0 +1,30 @@
+import streamlit as st
+from utils import hide_sidebar_page, login, sidebar_logged_in, SessionState
+def main():
+    st.set_page_config(page_title="Method", layout="wide")
+
+    st.markdown("""
+        <style>
+            margin-top: -220px;
+            section[data-testid="stSidebar"][aria-expanded="true"]{
+                display: none;
+            }
+        </style>
+        """, unsafe_allow_html=True)
+
+    hide_sidebar_page()
+
+    if 'session_state' not in st.session_state:
+        st.session_state.session_state = SessionState()
+
+    if not st.session_state.session_state.logged_in:
+        login()
+    else:
+        sidebar_logged_in()
+# PAGE CONTENT GOES HERE #################################################################
+        st.header(':rainbow[DASHBOARD]', divider='rainbow')
+        st.title(':green[Coming soon...]')
+
+
+if __name__ == "__main__":
+    main()
